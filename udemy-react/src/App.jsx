1dc5e550e3054ddfa45f9a722453e033108ba54e
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-02-18 12:35:05 +0900

    state practice

--- udemy-react/src/App.jsx
+++ udemy-react/src/App.jsx
@@ -5,11 +5,12 @@ import "./styles.css";
 
 export default function App() {
 
-  const [selectesTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
 
   function handleSelect(selectedButton) {
     tabContent = selectedButton;
-    console.log(tabContent);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
   }
 
   console.log('APP COMPONENT EXECUTING');
@@ -17,49 +18,33 @@ export default function App() {
 
   return (
     <div>
-      <Header>
-        <img src="react-logo-xs.png" alt="React logo" />
+      <Header/>
         <main>
-          <h1>React.js</h1>
-          <p>i.e., using the React library for rendering the UI</p>
-        </div>
-      </Header>
-
-      <div id="tabs">
-        <menu>
-          <button
-            className={activeContentIndex === 0 ? "active" : ""}
-            onClick={() => setActiveContentIndex(0)}
-          >
-            Why React?
-          </button>
-          <button
-            className={activeContentIndex === 1 ? "active" : ""}
-            onClick={() => setActiveContentIndex(1)}
-          >
-            Core Features
-          </button>
-          <button
-            className={activeContentIndex === 2 ? "active" : ""}
-            onClick={() => setActiveContentIndex(2)}
-          >
-            Related Resources
-          </button>
-          <button
-            className={activeContentIndex === 3 ? "active" : ""}
-            onClick={() => setActiveContentIndex(3)}
-          >
-            react vs js
-          </button>
-        </menu>
-        <div id="tab-content">
-          <ul>
-            {content[activeContentIndex].map((item) => (
-              <li key={item}>{item}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
+          <section id="core-concepts">
+            <h2>Core Concepts</h2>
+            <ul>
+              <CoreConcept
+                title = {CORE_CONCEPTS[0].title}
+                description= {CORE_CONCEPTS[0].description}
+                image={CORE_CONCEPTS[0].image}
+                />
+                <CoreConcept {...CORE_CONCEPTS[1]}/>
+                <CoreConcept {...CORE_CONCEPTS[2]}/>
+                <CoreConcept {...CORE_CONCEPTS[3]}/>
+            </ul>
+          </section>
+          <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+              <TapButton onSelect={()=>handleSelect('components')}>Components</TapButton>
+              <TapButton onSelect={()=>handleSelect('jsx')}>JSX</TapButton>
+              <TapButton onSelect={()=>handleSelect('props')}>Props</TapButton>
+              <TapButton onSelect={()=>handleSelect('state')}>State</TapButton>
+            </menu>
+            {selectedTopic}
+          </section>
+        </main>
+     </div>
   );
 }
+export default App;
